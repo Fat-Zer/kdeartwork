26bafe89f662d9929adca7d3452074a8b0d0e614
Golubev Alexander <fatzer2@gmail.com>
2011-10-15 18:08:04 +0400

    initial cmake support.  wallpepars added.

---
+++ ConfigureChecks.cmake
@@ -0,0 +1,18 @@
+#################################################
+#
+#  (C) 2011 Golubev Alexander
+#  fatzer2 (AT) gmail.com
+#
+#  Improvements and feedback are welcome
+#
+#  This file is released under GPL >= 2
+#
+#################################################
+
+#if( BUILD_KOPETE OR BUILD_KPPP )
+#  check_include_file( string.h HAVE_STRING_H )
+#endif( )
+
+# required stuff
+find_package( TQt )
+find_package( TDE )
